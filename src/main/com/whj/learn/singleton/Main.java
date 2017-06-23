dd65e677817b91d03c0b7c25c28185b42922ccdf
wuhaijun <826818399@qq.com>
2017-06-23 18:18:10 +0800

    设计模式学习

--- src/main/com/whj/learn/singleton/Main.java
+++ src/main/com/whj/learn/singleton/Main.java
@@ -1,8 +1,27 @@
 package main.com.whj.learn.singleton;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by wuhaijun on 2017/6/21.
  */
 public class Main {
+
+    @Test
+    /**单线程测试*/
+    public void test1(){
+        SingletonModel single1 = SingletonModel.newInstance();
+        SingletonModel single2 = SingletonModel.newInstance();
+        System.out.print(single1 == single2);
+    }
+
+    /**多线程测试*/
+    public void test2(){
+
+    }
+
 
 }
